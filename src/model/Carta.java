032ac9a627a1cc95006b9ee25739f06ae7c22b7b
Daniel Guimarães <61909415+Leinadium@users.noreply.github.com>
2021-04-26 08:48:40 -0300

    Update Carta.java
    Inicio do desenvolvimento das cartas

--- src/model/Carta.java
+++ src/model/Carta.java
@@ -1,5 +1,23 @@
+/**
+ * Classe carta.
+ * 
+ * Historico de versoes:
+ * - 26/04: Criado enums para a carta
+ */
+
 package model;
 
-public class Carta {
+enum CorCarta {
+	VERMELHO, PRETO;
+}
 
+enum NomeCarta {
+	AS, DOIS, TRES, QUATRO, CINCO, SEIS, SETE, OITO, NOVE, VALETE, DAMA, REI;
+}
+
+enum NaipeCarta {
+	COPAS, OUROS, ESPADAS, PAUS;
+}
+public class Carta {
+	// TODO
 }
